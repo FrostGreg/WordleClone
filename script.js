b988735ccc8dbb6ab2a39aaffd033169d6959804
FrostGreg <gregfrost305@gmail.com>
2022-10-06 12:07:30 +0100

    feat: line by line input and deletion

--- script.js
+++ script.js
@@ -18,13 +18,17 @@ window.addEventListener('keydown', (event) => {
     if (active && isLetter(event.key)){
         document.querySelector(`#letter-${parseInt(currentNumber)}`).textContent = event.key;
         
-        currentNumber >= 29 ? currentNumber : currentNumber++;
+        (currentNumber + 1) % 5 === 0 ? currentNumber : currentNumber++;
 
     }
-    else if (event.key === 'Backspace'){
-        document.querySelector(`#letter-${parseInt(currentNumber)}`).textContent = '';
-        currentNumber > 0 ? currentNumber-- : currentNumber;
+    else if (event.key === 'Backspace' && currentNumber % 5 !== 0){ 
+        if (document.querySelector(`#letter-${parseInt(currentNumber)}`).textContent === ''){
+            currentNumber--;
+        }
+            document.querySelector(`#letter-${parseInt(currentNumber)}`).textContent = '';
 
+    } else if (event.key === 'Enter' && (currentNumber+1) % 5 === 0){
+        currentNumber >= 29 ? currentNumber : currentNumber++;
     }
     console.log(currentNumber);
 
